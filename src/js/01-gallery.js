b8fc1b4130440356b3d3646410a976edbdf378da
annared <annared1982@gmail.com>
2022-12-08 11:47:59 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,9 +10,37 @@
 // import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-
+// import SimpleLightbox from "simplelightbox";
+// // Дополнительный импорт стилей
+// import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryREF = document.querySelector('.gallery');
+const gallery = addCard(galleryItems);
+galleryREF.innerHTML = gallery;
+
+function addCard(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return ` 
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"           
+            alt="${description}"
+            />
+        </a>
+    `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
