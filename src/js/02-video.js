127dca178a3787bf24bd244e7fc77981cb756b3b
annared <annared1982@gmail.com>
2022-12-08 13:33:16 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,3 +20,16 @@
 // Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
 // При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
 // Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+
+import Player from '@vimeo/player';
+
+iframeId = document.querySelector('#vimeo-player');
+console.log(iframeId);
+
+const player = new Player('handstick', {
+  id: 19231868,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
